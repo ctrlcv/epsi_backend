91f4cf2e689b620176cbf6c3b9855dcaa965feb2
Hyungoo Kim <ctrlcvis@gmail.com>
2022-01-24 11:11:44 +0900

    Work 210124

--- config/prod.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    HOST: 'my5701.gabiadb.com',
-    PORT: '3306',
-    USER: 'ecommid',
-    PASSWORD: 'ecomm9621!',
-    DATABASE: 'ecommdb',
-    secretOrKey: "KR.CO.ECOMMTECH",
-    secretOrRefreshKey: "EPSIECOMMTECHCOKR"
-}
